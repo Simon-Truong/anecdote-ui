62a515d22862bb4c68084fed315f7451c12e14c0
Simon Truong <simontruong2@gmail.com>
2020-04-06 08:34:43 +1000

    [refactor] changed routing architecture

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
-import routes from './routes';
+import router from './router/index';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-});
 
 new Vue({
   router,
