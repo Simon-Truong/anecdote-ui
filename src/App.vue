2e4f4f56dd90a6fcde69e4f0058ace57f7f86743
Simon Truong <simontruong2@gmail.com>
2020-04-07 08:21:36 +1000

    [feat] added app tool bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar app dark color="indigo">
+      <v-toolbar-title>
+        anecdote
+      </v-toolbar-title>
+    </v-app-bar>
+
     <v-content>
       <router-view />
     </v-content>
+
   </v-app>
 </template>
 
