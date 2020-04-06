ddeda3cec91e80ebba116e902e56d8b67e4b22c6
Simon Truong <simontruong2@gmail.com>
2020-04-07 07:56:12 +1000

    [feat] added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+
 export default {
   name: 'App',
-  components: {}
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style scoped>
-#app {
-}
-</style>
