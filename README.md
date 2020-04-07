98b3ca37ed732b0d6ba0d318134b3a9f54294e24
Simon Truong <me.simon_@hotmail.com>
2020-04-08 08:29:19 +1000

    [feat] added node-sass, initial README, created new component Appbar.vue and styling

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # anecdote-ui
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+# Styles from lightest to darkest
+- grey lighten-1 #BDBDBD
+- grey lighten-5 #FAFAFA
---
+++ src/components/Appbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar app dark color="teal darken-4">
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+          <v-toolbar-title>
+            <router-link to="/home" class="custom-anchor">
+              <span class="grey--text text--lighten-5">anecdote</span>
+            </router-link>
+          </v-toolbar-title>
+        </v-col>
+
+        <v-col cols="6" />
+
+        <v-col cols="3">
+          <div>
+            <router-link to="/about" class="custom-anchor mr-2">About</router-link>
+            <router-link to="/signup" class="custom-anchor mr-2">Sign Up</router-link>
+            <router-link to="/login" class="custom-anchor">Login</router-link>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Appbar'
+};
+</script>
+
+<style scoped lang="scss">
+.custom-anchor {
+  text-decoration: none;
+  cursor: pointer;
+  color: #BDBDBD;
+  transition-duration: 0.5s;
+
+    &:hover {
+      color: #FAFAFA;
+    }
+}
+</style>
