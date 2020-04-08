0892729817275df399e762d68dede2ba9b4eeb5f
Simon Truong <me.simon_@hotmail.com>
2020-04-09 08:04:55 +1000

    [style] fixed theme and added footer

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,27 +1,34 @@
 <template>
-  <v-app-bar app dark color="teal darken-4">
-    <v-container>
-      <v-row>
-        <v-col cols="3">
-          <v-toolbar-title>
-            <router-link to="/home" class="custom-anchor">
-              <span class="grey--text text--lighten-5">anecdote</span>
-            </router-link>
-          </v-toolbar-title>
-        </v-col>
+  <div>
+    <v-app-bar app light flat>
+      <v-container>
+        <v-row>
+          <v-col cols="3" />
 
-        <v-col cols="6" />
+          <v-col cols="3">
+            <v-toolbar-title>
+              <router-link to="/home" class="custom-anchor">
+                <span class="grey--text text--darken-4">anecdote</span>
+              </router-link>
+            </v-toolbar-title>
+          </v-col>
 
-        <v-col cols="3">
-          <div>
-            <router-link to="/about" class="custom-anchor mr-2">About</router-link>
-            <router-link to="/signup" class="custom-anchor mr-2">Sign Up</router-link>
-            <router-link to="/login" class="custom-anchor">Login</router-link>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app-bar>
+          <v-col cols="3">
+            <div class="text-right">
+              <router-link to="/about" class="custom-anchor mr-2">About</router-link>
+              <router-link to="/login" class="custom-anchor">Login</router-link>
+            </div>
+          </v-col>
+
+          <v-col cols="3" />
+        </v-row>
+
+        <v-row>
+          <v-divider />
+        </v-row>
+      </v-container>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
@@ -34,11 +41,11 @@ export default {
 .custom-anchor {
   text-decoration: none;
   cursor: pointer;
-  color: #BDBDBD;
+  color: #757575;
   transition-duration: 0.5s;
 
-    &:hover {
-      color: #FAFAFA;
-    }
+  &:hover {
+    color: #424242;
+  }
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-footer app light>
+      <v-col class="grey--text text--darken-4 text-center" cols="12">
+        anecdote © 2020
+      </v-col>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+<style>
+</style>
