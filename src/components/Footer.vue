a0697104c7ee60d0895015072938b3342f5c5432
Simon Truong <me.simon_@hotmail.com>
2020-04-11 19:39:48 +1000

    [feat] customized items per page for data table and changed footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,13 @@
     <v-footer color="white" padless>
       <v-container>
         <v-row>
-          <v-col col="12" class="py-0 d-flex justify-end">
-            <div class="grey--text text--darken-4">&copy; {{ new Date().getFullYear() }}</div>
+          <v-col cols="3" />
+
+          <v-col cols="6" class="pt-0 d-flex justify-end">
+            <router-link to="/feedback" class="custom-anchor body-2">Feedback?</router-link>
           </v-col>
+
+          <v-col cols="3" />
         </v-row>
       </v-container>
     </v-footer>
