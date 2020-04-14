dbc670b6fd425aba368ea51bf909db4c9fe52b03
Simon Truong <simontruong2@gmail.com>
2020-04-14 20:51:18 +1000

    [feat] implemented front-end validation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,6 +22,7 @@
                 persistent-hint
                 dense
                 autofocus
+                validate-on-blur
                 @click:append="search"
                 @keydown.enter="search"
                 v-model="searchQuery"
@@ -35,29 +36,57 @@
             <v-container class="pt-0">
               <v-row>
                 <v-col cols="3">
-                  <v-text-field dense class="pt-0" label="First Name"></v-text-field>
+                  <v-text-field
+                    dense
+                    class="pt-0"
+                    label="First Name"
+                    v-model="firstName"
+                    :rules="[rules.required]"
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="3">
-                  <v-text-field dense class="pt-0" label="Last Name"></v-text-field>
+                  <v-text-field
+                    dense
+                    class="pt-0"
+                    label="Surname"
+                    v-model="surname"
+                    :rules="[rules.required]"
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col cols="6" class="pt-0">
-                  <v-text-field dense class="pt-0" label="Email"></v-text-field>
+                  <v-text-field
+                    dense
+                    class="pt-0"
+                    label="Email"
+                    v-model="email"
+                    :rules="[rules.required, rules.email]"
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col cols="6" class="pt-0">
-                  <v-text-field dense class="pt-0" label="Password" :type="'password'" counter></v-text-field>
+                  <v-text-field
+                    dense
+                    class="pt-0"
+                    label="Password"
+                    v-model="password"
+                    :type="showPassword ? 'text': 'password'"
+                    :append-icon="showPassword ? 'mdi-eye': 'mdi-eye-off'"
+                    counter
+                    @click:append="showPassword = !showPassword"
+                    :rules="[rules.required, rules.minimumLength, rules.password]"
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col cols="6" class="d-flex justify-end">
-                  <v-btn tile depressed small dark color="#1976d2">Sign up</v-btn>
+                  <v-btn tile depressed small dark @click="submit()" color="#1976d2">Sign up</v-btn>
                 </v-col>
               </v-row>
             </v-container>
@@ -73,7 +102,51 @@ export default {
   name: 'Home',
   data() {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      showPassword: false,
+      firstName: '',
+      surname: '',
+      email: '',
+      password: '',
+      rules: {
+        required: value => !!value || 'Required',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid e-mail.';
+        },
+        minimumLength: value => value.length >= 8 || 'Password must be at least 8 characters long.',
+        password: value => {
+          const upperCasePattern = /[A-Z]/;
+          const lowerCasePattern = /[a-z]/;
+          const numberPattern = /[0-9]/;
+          const specialCharacterPattern = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
+
+          if (!upperCasePattern.test(value)) {
+            return 'Password must contain one uppercase word.';
+          }
+
+          if (!lowerCasePattern.test(value)) {
+            return 'Password must contain one lowercase word.';
+          }
+
+          if (!numberPattern.test(value)) {
+            return 'Password must contain one number.';
+          }
+
+          if (!specialCharacterPattern.test(value)) {
+            return 'Password must contain one special character.';
+          }
+
+          if (
+            value.includes(this.firstName) ||
+            value.includes(this.surname) ||
+            value.includes(this.email)) {
+            return 'Password must not contain first name, surname or email.';
+          }
+
+          return true;
+        }
+      }
     };
   },
   methods: {
@@ -87,6 +160,9 @@ export default {
       };
 
       this.$router.push(routeOptions);
+    },
+    submit() {
+      console.log(this.firstName, this.surname, this.email, this.password);
     }
   }
 };
