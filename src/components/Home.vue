32f08ba591f26211fbebc61c8aa8ae51d25bc824
Simon Truong <simontruong2@gmail.com>
2020-05-04 08:19:08 +1000

    [style] changed sign up form aesthetics

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,6 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="6" />
-
-        <v-col cols="6">
-          <h2 class="pl-3" v-if="!isAuthenticated">sign up</h2>
-        </v-col>
-      </v-row>
-
-      <v-row>
         <v-col cols="6" class="d-flex justify-end">
           <v-row>
             <v-col cols="6" />
@@ -31,133 +23,138 @@
           </v-row>
         </v-col>
 
-        <v-col cols="6">
-          <v-form v-if="!isAuthenticated" v-model="isFormValid" ref="form">
-            <v-container class="pt-0">
-              <v-row>
-                <v-col cols="3">
-                  <v-text-field
-                    dense
-                    class="pt-0"
-                    label="First Name"
-                    v-model="firstName"
-                    :rules="[rules.required]"
-                    @keydown.enter="submit"
-                  ></v-text-field>
-                </v-col>
+        <v-col cols="3">
+          <fieldset>
+            <legend>
+              <em>Sign up</em>
+            </legend>
+            <v-form v-if="!isAuthenticated" v-model="isFormValid" ref="form">
+              <v-container class="pt-0">
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field
+                      dense
+                      class="pt-0"
+                      label="First Name"
+                      v-model="firstName"
+                      :rules="[rules.required]"
+                      @keydown.enter="submit"
+                    ></v-text-field>
+                  </v-col>
 
-                <v-col cols="3">
-                  <v-text-field
-                    dense
-                    class="pt-0"
-                    label="Surname"
-                    v-model="surname"
-                    :rules="[rules.required]"
-                    @keydown.enter="submit"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
+                  <v-col cols="6">
+                    <v-text-field
+                      dense
+                      class="pt-0"
+                      label="Surname"
+                      v-model="surname"
+                      :rules="[rules.required]"
+                      @keydown.enter="submit"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="6" class="pt-0">
-                  <v-text-field
-                    dense
-                    class="pt-0"
-                    label="Email"
-                    v-model="email"
-                    :rules="[rules.required, rules.email]"
-                    @keydown.enter="submit"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
+                <v-row>
+                  <v-col cols="12" class="pt-0">
+                    <v-text-field
+                      dense
+                      class="pt-0"
+                      label="Email"
+                      v-model="email"
+                      :rules="[rules.required, rules.email]"
+                      @keydown.enter="submit"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="6" class="pt-0">
-                  <v-text-field
-                    dense
-                    class="pt-0"
-                    label="Password"
-                    v-model="password"
-                    :type="showPassword ? 'text': 'password'"
-                    :append-icon="showPassword ? 'mdi-eye': 'mdi-eye-off'"
-                    counter
-                    @click:append="showPassword = !showPassword"
-                    :rules="[rules.required, rules.minimumLength, rules.password]"
-                    @keydown.enter="submit"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
+                <v-row>
+                  <v-col cols="12" class="pt-0">
+                    <v-text-field
+                      dense
+                      class="pt-0"
+                      label="Password"
+                      v-model="password"
+                      :type="showPassword ? 'text': 'password'"
+                      :append-icon="showPassword ? 'mdi-eye': 'mdi-eye-off'"
+                      counter
+                      @click:append="showPassword = !showPassword"
+                      :rules="[rules.required, rules.minimumLength, rules.password]"
+                      @keydown.enter="submit"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="6" class="pt-0">
-                  <v-text-field
-                    dense
-                    class="pt-0"
-                    label="Confirm Password"
-                    v-model="confirmPassword"
-                    counter
-                    :rules="[rules.required, rules.confirmPassword]"
-                    type="password"
-                    @keydown.enter="submit"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
+                <v-row>
+                  <v-col cols="12" class="pt-0">
+                    <v-text-field
+                      dense
+                      class="pt-0"
+                      label="Confirm Password"
+                      v-model="confirmPassword"
+                      counter
+                      :rules="[rules.required, rules.confirmPassword]"
+                      type="password"
+                      @keydown.enter="submit"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="6" class="py-0">
-                  <v-combobox
-                    class="pt-0"
-                    dense
-                    label="Tags"
-                    outlined
-                    multiple
-                    hide-selected
-                    :items="items"
-                    v-model="tags"
-                    :search-input.sync="searchInput"
-                    hint="Describe yourself"
-                    persistent-hint
-                  >
-                    <template v-slot:no-data>
-                      <v-list-item>
-                        <v-list-item-content>
-                          <v-list-item-title>
-                            No results matching "
-                            <strong>{{ searchInput }}</strong>". Press
-                            <kbd>enter</kbd> to create a new one
-                          </v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </template>
+                <v-row>
+                  <v-col cols="12" class="py-0">
+                    <v-combobox
+                      class="pt-0"
+                      dense
+                      label="Tags"
+                      outlined
+                      multiple
+                      hide-selected
+                      :items="items"
+                      v-model="tags"
+                      :search-input.sync="searchInput"
+                      hint="Describe yourself"
+                      persistent-hint
+                    >
+                      <template v-slot:no-data>
+                        <v-list-item>
+                          <v-list-item-content>
+                            <v-list-item-title>
+                              No results matching "
+                              <strong>{{ searchInput }}</strong>". Press
+                              <kbd>enter</kbd> to create a new one
+                            </v-list-item-title>
+                          </v-list-item-content>
+                        </v-list-item>
+                      </template>
 
-                    <template v-slot:selection="{ attrs, item, parent, selected }">
-                      <v-chip v-bind="attrs" :input-value="selected" label small>
-                        <span class="pr-2">{{ item }}</span>
-                        <v-icon small @click="parent.selectItem(item)">fas fa-times</v-icon>
-                      </v-chip>
-                    </template>
-                  </v-combobox>
-                </v-col>
-              </v-row>
+                      <template v-slot:selection="{ attrs, item, parent, selected }">
+                        <v-chip v-bind="attrs" :input-value="selected" label small>
+                          <span class="pr-2">{{ item }}</span>
+                          <v-icon small @click="parent.selectItem(item)">fas fa-times</v-icon>
+                        </v-chip>
+                      </template>
+                    </v-combobox>
+                  </v-col>
+                </v-row>
 
-              <v-row>
-                <v-col cols="6" class="d-flex justify-end">
-                  <span v-if="showSpinner" class="d-flex align-center mr-2">
-                    <v-progress-circular color="#757575" indeterminate size="20"></v-progress-circular>
-                  </span>
-                  <v-btn
-                    tile
-                    depressed
-                    small
-                    dark
-                    :disabled="!isFormValid"
-                    @click="submit"
-                    color="#1976d2"
-                  >Sign up</v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
+                <v-row>
+                  <v-col cols="12" class="d-flex justify-end">
+                    <span v-if="showSpinner" class="d-flex align-center mr-2">
+                      <v-progress-circular color="#757575" indeterminate size="20"></v-progress-circular>
+                    </span>
+                    <v-btn
+                      tile
+                      depressed
+                      small
+                      dark
+                      :disabled="!isFormValid"
+                      @click="submit"
+                      color="#1976d2"
+                    >Sign up</v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </fieldset>
         </v-col>
       </v-row>
     </v-container>
@@ -291,4 +288,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/_variables.scss';
+
+fieldset {
+  border-color: $background-color;
+}
 </style>
