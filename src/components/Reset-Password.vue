0275e87b230d750876e0ac0c4cb5145c809aca19
Simon Truong <simontruong2@gmail.com>
2020-05-09 20:34:15 +1000

    [feat] implemented password reset

--- src/components/Reset-Password.vue
+++ src/components/Reset-Password.vue
@@ -1,17 +1,32 @@
 <template>
- <div>
-     <v-form v-model="isFormValid" ref="form">
-        <v-container>
+  <div>
+    <v-form v-model="isFormValid" ref="form">
+      <v-container>
         <v-row>
-            <v-col cols="12" class="d-flex justify-center">
+          <v-col cols="12" class="d-flex justify-center">
             <v-card light flat tile min-height="200" min-width="800">
-                <v-card-title>Reset Password</v-card-title>
-                <v-card-subtitle class="pb-0">Enter your new password</v-card-subtitle>
-                <v-card-text>
+              <v-card-title>Reset Password</v-card-title>
+              <v-card-subtitle class="pb-0">Enter your code and new password</v-card-subtitle>
+              <v-card-text>
                 <v-container class="pl-0 pb-0">
-                    <v-row>
+                  <v-row>
                     <v-col cols="4">
-                        <v-text-field
+                      <v-text-field
+                        class="custom-text-field"
+                        dense
+                        outlined
+                        label="Enter your code here"
+                        @keyup.enter="resetPassword"
+                        v-model="secretCode"
+                        persistent-hint
+                        hint="Include dashes"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+
+                  <v-row>
+                    <v-col class="pt-5" cols="4">
+                      <v-text-field
                         dense
                         label="New Password"
                         @keyup.enter="resetPassword"
@@ -21,13 +36,13 @@
                         counter
                         @click:append="showPassword = !showPassword"
                         :rules="[rules.required, rules.minimumLength, rules.password]"
-                        ></v-text-field>
+                      ></v-text-field>
                     </v-col>
-                    </v-row>
+                  </v-row>
 
-                    <v-row>
+                  <v-row>
                     <v-col class="pt-0" cols="4">
-                        <v-text-field
+                      <v-text-field
                         dense
                         label="Confirm Password"
                         @keyup.enter="resetPassword"
@@ -35,21 +50,29 @@
                         counter
                         :rules="[rules.required, rules.confirmPassword]"
                         type="password"
-                        ></v-text-field>
+                      ></v-text-field>
                     </v-col>
-                    </v-row>
+                  </v-row>
                 </v-container>
 
-                <v-btn tile depressed small dark color="#1976d2" @click="resetPassword" :disabled="!isFormValid">Reset Password</v-btn>
+                <v-btn
+                  tile
+                  depressed
+                  small
+                  dark
+                  color="#1976d2"
+                  @click="resetPassword"
+                  :disabled="!isFormValid"
+                >Reset Password</v-btn>
                 <span v-if="showSpinner" class="ml-2">
-                    <v-progress-circular color="#757575" indeterminate size="20"></v-progress-circular>
+                  <v-progress-circular color="#757575" indeterminate size="20"></v-progress-circular>
                 </span>
-                </v-card-text>
+              </v-card-text>
             </v-card>
-            </v-col>
+          </v-col>
         </v-row>
-        </v-container>
-     </v-form>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
@@ -58,34 +81,60 @@ import rulesMixins from '../mixins/rules.mixins';
 import apiClient from '../service/user.service';
 
 export default {
-    name: 'ResetPassword',
-    mixins: [rulesMixins],
-    data() {
-        return {
-            confirmPassword: '',
-            showPassword: false,
-            isFormValid: false,
-            showSpinner: false
-        }
+  name: 'ResetPassword',
+  mixins: [rulesMixins],
+  data() {
+    return {
+      confirmPassword: '',
+      showPassword: false,
+      isFormValid: false,
+      secretCode: '',
+      showSpinner: false,
+      userId: this.$route.params.id
+    };
+  },
+  methods: {
+    resetPassword() {
+      if (!this.secretCode) {
+        return;
+      }
+
+      this.showSpinner = true;
+
+      const body = {
+        userId: this.userId,
+        secretCode: this.secretCode,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      };
+
+      apiClient
+        .resetPassword(body)
+        .then(response => {
+          this.processSuccessResponse(response.data);
+          this.$router.push('/login');
+        })
+        .catch(error => this.processErrorResponse(error.response))
+        .finally(() => (this.showSpinner = false));
     },
-    methods: {
-        resetPassword() {
-            this.showSpinner = true;
+    processSuccessResponse(message) {
+      alert(message);
+    },
+    processErrorResponse(response) {
+      const status = response.status;
 
-            const body = {
-                password: this.password,
-                confirmPassword: this.confirmPassword
-            }
+      if (status === 500) {
+        console.log(response.data);
+        return;
+      }
 
-            apiClient.resetPassword(body)
-                .then(response => console.log({response}))
-                .catch(error => console.log({error}))
-                .finally(() => this.showSpinner = false);
-        }
+      if (status === 400) {
+        alert(response.data);
+      }
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
