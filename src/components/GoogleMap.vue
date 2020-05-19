2cd9165b8035a02956f495c3547219a765c6f02b
Simon Truong <simontruong2@gmail.com>
2020-05-20 08:25:36 +1000

    [style] fixed style of google maps inputs and button and remove multiple markers and places

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -2,18 +2,16 @@
   <div>
     <div>
       <label>
-        <gmap-autocomplete placeholder="Search" @place_changed="setPlace"></gmap-autocomplete>
-        <v-btn tile depressed small dark color="#1976d2" @click="addMarker">Select</v-btn>
+        <gmap-autocomplete class="input" placeholder="Search" @place_changed="setPlace"></gmap-autocomplete>
+        <button class="button ml-2" @click="addMarker">Select</button>
       </label>
       <br />
     </div>
     <br />
     <gmap-map :center="center" :zoom="12" style="width:100%;  height: 400px;">
       <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
+        :position="marker.position"
+        @click="center=marker.position"
       ></gmap-marker>
     </gmap-map>
   </div>
@@ -27,16 +25,14 @@ export default {
       // default to Montreal to keep it simple
       // change this to whatever makes sense
       center: { lat: 45.508, lng: -73.587 },
-      markers: [],
-      places: [],
+      marker: {},
+      place: {},
       currentPlace: null
     };
   },
-
   mounted() {
     this.geolocate();
   },
-
   methods: {
     // receives a place object via the autocomplete component
     setPlace(place) {
@@ -48,8 +44,8 @@ export default {
           lat: this.currentPlace.geometry.location.lat(),
           lng: this.currentPlace.geometry.location.lng()
         };
-        this.markers.push({ position: marker });
-        this.places.push(this.currentPlace);
+        this.marker = { position: marker };
+        this.place = { position: marker };
         this.center = marker;
         this.currentPlace = null;
       }
@@ -61,7 +57,59 @@ export default {
           lng: position.coords.longitude
         };
       });
-    }
+    },
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.button {
+  -webkit-appearance: button;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: buttontext;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: center;
+  align-items: flex-start;
+  cursor: default;
+  background-color: buttonface;
+  box-sizing: border-box;
+  margin: 0em;
+  font: 400 13.3333px Arial;
+  padding: 1px 6px;
+  border-width: 2px;
+  border-style: outset;
+  border-color: buttonface;
+  border-image: initial;
+}
+
+.input {
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  color: -internal-light-dark-color(black, white);
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  -webkit-appearance: textfield;
+  background-color: -internal-light-dark-color(white, black);
+  -webkit-rtl-ordering: logical;
+  cursor: text;
+  margin: 0em;
+  font: 400 13.3333px Arial;
+  padding: 1px 0px;
+  border-width: 2px;
+  border-style: inset;
+  border-color: initial;
+  border-image: initial;
+}
+
+</style>
